2c98ab3e7746ab9b194e4a865ef89da496fdc894
bluemario8 <bluemario8@yahoo.com>
2024-11-08 15:34:04 -0500

    Added header.js and footer.js to insert the header and footer html dynamically instead of copying them to everything

---
+++ footer.js
@@ -0,0 +1,42 @@
+let footerHtml = `
+    <div class="flex row">
+        <ul class="footer-list flex flex-left">
+            <li>
+                <a href="menu.html">Menu</a>
+            </li>
+            <li>
+                <a href="#">Rewards</a>
+            </li>
+            <li>
+                <a href="#">Reviews</a>
+            </li>
+        </ul>
+        <ul class="footer-list flex flex-right">
+            <li>
+                <a href="https://youtube.com">
+                    <ion-icon name="logo-youtube"></ion-icon>
+                </a>
+            </li>
+            <li>
+                <a href="https://x.com">
+                    <ion-icon name="logo-twitter"></ion-icon>
+                </a>
+            </li>
+            <li>
+                <a href="https://instagram.com">
+                    <ion-icon name="logo-instagram"></ion-icon>
+                </a>
+            </li>
+            <li>
+                <a href="https://facebook.com">
+                    <ion-icon name="logo-facebook"></ion-icon>
+                </a>
+            </li>
+        </ul>
+    </div>
+`;
+
+// let footerElem = document.getElementById("footer");
+document.getElementById("footer").innerHTML = footerHtml;
+document.getElementById("footer").classList = "section nunito-sans";
+
