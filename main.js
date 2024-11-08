917fdc43cbbb53e28a34afb00e973f8a3fc76524
Jacob Rushinski <dsrush2@verizon.net>
2024-11-08 10:52:27 -0500

    js logic of sign up complete v1

--- main.js
+++ main.js
@@ -13,6 +13,18 @@ function getForm() {
     lastName
   ];
 
+  // Creating a class allows you to store related variables in one single variable
+  class User {
+    // These are the 4 paramaters of "User" you have them in the constructor so you can reuse them throughout the code
+    constructor(firstName, lastName, password, email) {
+      // "this.firstName" sets "firstName" as the parameters value for this instance 
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.password = password;
+      this.email = email;
+    }
+  }
+
   if (checkNull.some(fieldInput => fieldInput === "")) {
     alert('Please fill in all fields');
     return;
@@ -26,23 +38,32 @@ function getForm() {
     const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     const specialChars = ['!', '@', '#', '$', '%'];
 
+    // 'numbers.some' looks through all the elements in the numbers array, 'num' is whatever 'numbers.some' is we point to 'password.includes(num)' to then check password to see if it containers any of those specific numbers
     const numCheck = numbers.some(num => password.includes(num));
     const specialCharCheck = specialChars.some(specChar => password.includes(specChar));
 
-    if (!/[a-zA-Z]/.test(password)) {
-      alert('Your password must contain atleast one letter');
+    if (password.length < 8) {
+      alert('Your password must be a length of 8 characters or greater');
+      return;
+      // "/" these slashes make it a regular expression, "[a-zA-Z]" this is a character class and matches everything that falls in the expression
+    } else if (!/[a-zA-Z]/.test(password)) { 
+      alert('Your password must contain at least one letter');
       return;
     } else if (!numCheck) {
-      alert('Your password must contain atleast one number');
+      alert('Your password must contain at least one number');
       return;
     } else if (!specialCharCheck) {
       alert('Your password must contain at least one special character: "!", "@", "#", "$", "%"');
       return;
-    } else if (password.length < 8) {
-      alert('Your password must be a length of 8 characters or greater');
-      return;
     }
     
-    localStorage.setItem(email, email);
+    // Creates a new variable with the Class "User". It also gets its parameters which are now what ever the users input was for each specific field
+    const user = new User(firstName, lastName, password, email);
+    // Stores the variable user which contains all the info of the account creation into the local storage. It has the email as the key 
+    // and "JSON.stringify(user)" converts the user variable to a JSON allowing us the store things in the local storage similar to an array, with 
+    // the email being the array name and all the constructor parameters being the content of that array
+    localStorage.setItem(email, JSON.stringify(user));
+    
+    location.href="index.html";
   }
 }
