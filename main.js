03712eb253235a77a3cee27b37147b8025905788
Jacob Rushinski <dsrush2@verizon.net>
2024-11-09 13:54:25 -0500

    Password required fields active updates: to do, add psedeu code

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialChars = ['!', '@', '#', '$', '%'];
+
+// Getting the password id (This will contain the value of the password/what the password is)
+const passwordId = document.getElementById('password');
+// Gets the ids for each visual password requirment check
+const passChangeText1 = document.getElementById('pass-change-text-1');
+const passChangeImg1 = document.getElementById('pass-change-img-1');
+const passChangeText2 = document.getElementById('pass-change-text-2');
+const passChangeImg2 = document.getElementById('pass-change-img-2');
+const passChangeText3 = document.getElementById('pass-change-text-3');
+const passChangeImg3 = document.getElementById('pass-change-img-3');
+const passChangeText4 = document.getElementById('pass-change-text-4');
+const passChangeImg4 = document.getElementById('pass-change-img-4');
+
+// Adds an event listener to password this means that whenever something is typed into password this function will trigger
+passwordId.addEventListener('input', function() {
+  // Gets the value of the password from the passwordId
+  const passwordValue = passwordId.value;
+
+  // Checks if these requirments are true or false, which ever they are that is then what the variable will equal
+  const lengthCheck = passwordValue.length >= 8;
+  const letterCheck = /[a-zA-Z]/.test(passwordValue);
+  const numCheck = numbers.some(num => passwordValue.includes(num));
+  const specialCharCheck = specialChars.some(specChar => passwordValue.includes(specChar));
+
+  // This changes the color for the word if it is true i
+  passChangeText1.style.color = lengthCheck ? 'green' : 'red';
+  passChangeImg1.src = lengthCheck ? 'images/green-check.png' : 'images/red-x.png';
+
+  passChangeText2.style.color = letterCheck ? 'green' : 'red';
+  passChangeImg2.src = letterCheck ? 'images/green-check.png' : 'images/red-x.png';
+
+  passChangeText3.style.color = numCheck ? 'green' : 'red';
+  passChangeImg3.src = numCheck ? 'images/green-check.png' : 'images/red-x.png';
+
+  passChangeText4.style.color = specialCharCheck ? 'green' : 'red';
+  passChangeImg4.src = specialCharCheck ? 'images/green-check.png' : 'images/red-x.png';
+});
+
 function getForm() {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
@@ -35,9 +75,6 @@ function getForm() {
     alert('The passwords do not match');
     return;
   } else {
-    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    const specialChars = ['!', '@', '#', '$', '%'];
-
     // 'numbers.some' looks through all the elements in the numbers array, 'num' is whatever 'numbers.some' is we point to 'password.includes(num)' to then check password to see if it containers any of those specific numbers
     const numCheck = numbers.some(num => password.includes(num));
     const specialCharCheck = specialChars.some(specChar => password.includes(specChar));
@@ -59,9 +96,9 @@ function getForm() {
     
     // Creates a new variable with the Class "User". It also gets its parameters which are now what ever the users input was for each specific field
     const user = new User(firstName, lastName, password, email);
-    // Stores the variable user which contains all the info of the account creation into the local storage. It has the email as the key 
-    // and "JSON.stringify(user)" converts the user variable to a JSON allowing us the store things in the local storage similar to an array, with 
-    // the email being the array name and all the constructor parameters being the content of that array
+    /* Stores the variable user which contains all the info of the account creation into the local storage. It has the email as the key 
+    and "JSON.stringify(user)" converts the user variable to a JSON allowing us the store things in the local storage similar to an array, with 
+    the email being the array name and all the constructor parameters being the content of that array */ 
     localStorage.setItem(email, JSON.stringify(user));
     // Takes you to home page and you are signed in
     location.href="index.html";
