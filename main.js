ea7d1dbaa390d4d77a328cd108a632ed28aac040
Jacob Rushinski <dsrush2@verizon.net>
2024-11-11 09:38:39 -0500

    sign up page complete v2

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ passwordId.addEventListener('input', function() {
   const numCheck = numbers.some(num => passwordValue.includes(num));
   const specialCharCheck = specialChars.some(specChar => passwordValue.includes(specChar));
 
-  // This changes the color for the word if it is true i
+  // Shorthand if else statements, if "lengthCheck" is true green is activated and if its false red is
   passChangeText1.style.color = lengthCheck ? 'green' : 'red';
   passChangeImg1.src = lengthCheck ? 'images/green-check.png' : 'images/red-x.png';
 
@@ -66,41 +66,78 @@ function getForm() {
   }
 
   if (checkNull.some(fieldInput => fieldInput === "")) {
-    alert('Please fill in all fields');
-    return;
+    invalidSignUp('empty field');
   } else if (localStorage.getItem(email) !== null) {
-    alert('This email is already in use');
-    return;
+    invalidSignUp('email used');
   } else if (password !== reEnterPassword) {
-    alert('The passwords do not match');
-    return;
+    invalidSignUp('pass dont match');
   } else {
     // 'numbers.some' looks through all the elements in the numbers array, 'num' is whatever 'numbers.some' is we point to 'password.includes(num)' to then check password to see if it containers any of those specific numbers
     const numCheck = numbers.some(num => password.includes(num));
     const specialCharCheck = specialChars.some(specChar => password.includes(specChar));
 
     if (password.length < 8) {
-      alert('Your password must be a length of 8 characters or greater');
-      return;
+      invalidSignUp('length of 8');
       // "/" these slashes make it a regular expression, "[a-zA-Z]" this is a character class and matches everything that falls in the expression
     } else if (!/[a-zA-Z]/.test(password)) { 
-      alert('Your password must contain at least one letter');
-      return;
+      invalidSignUp('1 letter');
     } else if (!numCheck) {
-      alert('Your password must contain at least one number');
-      return;
+      invalidSignUp('1 number');
     } else if (!specialCharCheck) {
-      alert('Your password must contain at least one special character: "!", "@", "#", "$", "%"');
-      return;
+      invalidSignUp('1 special char');
+    } else {
+      // Creates a new variable with the Class "User". It also gets its parameters which are now what ever the users input was for each specific field
+      const user = new User(firstName, lastName, password, email);
+      /* Stores the variable user which contains all the info of the account creation into the local storage. It has the email as the key 
+      and "JSON.stringify(user)" converts the user variable to a JSON allowing us the store things in the local storage similar to an array, with 
+      the email being the array name and all the constructor parameters being the content of that array */ 
+      localStorage.setItem(email, JSON.stringify(user));
+      // Takes you to home page and you are signed in
+      location.href="index.html";
     }
-    
-    // Creates a new variable with the Class "User". It also gets its parameters which are now what ever the users input was for each specific field
-    const user = new User(firstName, lastName, password, email);
-    /* Stores the variable user which contains all the info of the account creation into the local storage. It has the email as the key 
-    and "JSON.stringify(user)" converts the user variable to a JSON allowing us the store things in the local storage similar to an array, with 
-    the email being the array name and all the constructor parameters being the content of that array */ 
-    localStorage.setItem(email, JSON.stringify(user));
-    // Takes you to home page and you are signed in
-    location.href="index.html";
   }
+}
+
+function showPassword() {
+  const showPass = document.getElementById('password');
+  const showRePass = document.getElementById('re-password');
+  const checkBox = document.getElementById('show-pass');
+
+  // "isChecked" will be true or false, if its checked it will be true if not it will be false
+  const isChecked = checkBox.checked;
+  
+  // Shorthand if else statement
+  showPass.type = isChecked ? 'text' : 'password';
+  showRePass.type = isChecked ? 'text' : 'password';
+}
+
+function invalidSignUp(errorType) {
+  let errorText = '';
+  switch (errorType) {
+    case 'empty field':
+      errorText = 'Please fill in all fields';
+      break;
+    case 'email used':
+      errorText = 'Email is already in use';
+      break;
+    case 'pass dont match': 
+      errorText = 'Passwords do not match';
+      break;
+    case 'length of 8':
+      errorText = 'Passwords must be a length of atleast 8 characters';
+      break;
+    case '1 letter':
+      errorText = 'Passwords must contain atleast 1 letter';
+      break;
+    case '1 number':
+      errorText = 'Passwords must contain atleast 1 number';
+      break;
+    case '1 special char': 
+      errorText = 'Passwords must contain atleast 1: "!", "@", "#", "$", "%"';
+      break;
+  }
+  const invalidSignUpLocation = document.getElementById('invalid-signup-popup-loc');
+  const content = document.getElementById('invalid-signup-ptag');
+  invalidSignUpLocation.style.display = 'flex';
+  content.innerText = errorText;
 }
