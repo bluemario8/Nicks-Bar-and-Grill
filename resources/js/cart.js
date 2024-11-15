1e615af131359af1fe8c66d2b8f03b68fab8dd8d
Jacob Rushinski <dsrush2@verizon.net>
2024-11-15 10:12:30 -0500

    cart rewards progress

--- resources/js/cart.js
+++ resources/js/cart.js
@@ -1,11 +1,11 @@
 function displayCart() {
-  // This is the HTML that will be inserted into the code when the cart btn is clicked
+  const checkLogged = localStorage.getItem('loggedIn');
+  let cartRewardsHtml = ''; // Default to an empty string to avoid errors
+
+  // Base cart HTML structure
   const cartHtml = `
-    <!-- All content of the cart page -->
     <div id="cart-page">  
-      <!-- Just the right side container of the cart -->   
       <div id="cart-buy">
-        <!-- Header of the cart -->
         <div class="top-cart flex"> 
           <div class="flex">
             <ion-icon name="close-outline" class="pointer" id="close-cart" onclick="closeCart()"></ion-icon>
@@ -14,23 +14,10 @@ function displayCart() {
             <h2 class="weight-9">Cart</h2>
           </div>
         </div>
-        <!-- Rewards of the cart -->
-        <div class="rewards-cart flex">
-          <p>rewards</p>
-        </div>
-        <!-- Pickup/delivery of the cart -->
-        <div class="delivery-pickup-cart">
-          <p>get type</p>
-        </div>
-        <!-- My order of the cart -->
-        <div class="my-order-cart flex">
-          <p>my order</p>
-        </div>
-        <!-- Body of the cart -->
-        <div class="body-cart flex"> 
-          <p>body</p>
-        </div>
-        <!-- Total & to checkout part of the cart -->
+        <div id="rewards-cart" class="rewards-cart flex"></div>
+        <div class="delivery-pickup-cart"><p>get type</p></div>
+        <div class="my-order-cart flex"><p>my order</p></div>
+        <div class="body-cart flex"><p>body</p></div>
         <div class="footer-cart flex">
           <button onclick="" id="checkout-btn">Checkout</button>
         </div>
@@ -38,22 +25,57 @@ function displayCart() {
     </div>
   `;
 
-  // This gets the cart element location to insert the code
+  // Determine the rewards content based on login status
+  switch (checkLogged) {
+    case 'guest':
+      console.log('signed in as guest');
+      cartRewardsHtml = `<p class="size-4">Welcome Guest! Enjoy browsing our store.</p>`;
+      break;
+    case null:
+      console.log('not signed in');
+      cartRewardsHtml = `
+        <div class="rewards-cart-split column">
+          <div class="rewards-cart-header">
+            <div class="flex">
+              <img src="images/basic/logo-no-background.png" id="cart-logo">
+            </div>
+            <div class="flex column align-items-center">
+              <h5>Nick's Points & Rewards!</h5>
+              <p class="size-4">Sign up and get <b>200 points</b> for free!</p>
+            </div>
+          </div>
+          <div class="rewards-cart-body">
+            <p class="size-2">To gain points on every purchase, <b>Login</b> or <b>Sign Up</b>! Points can be redeemed for <b>money off your order</b>!</p>
+          </div>
+        </div>
+        <div class="rewards-cart-login flex column">
+          <button onclick="window.location.href='signUp.html';" class="cart-sign-up pointer roboto-slab">Join</button>
+          <a onclick="window.location.href='login.html'" class="cart-login weight-5 pointer roboto-slab">Sign In</a>
+        </div>
+      `;
+      break;
+    default:
+      console.log('signed in');
+      cartRewardsHtml = `<p class="size-4">You're signed in. Earn rewards with every purchase!</p>`;
+      break;
+  }
+
+  // Insert the main cart structure into the page
   const page = document.getElementById('cart');
-  // This inserts the code
   page.innerHTML = cartHtml;
 
-  /* "setTimeout" is a built in js function, this function will delay when the function is called
-  Delaying the function is necessary so the page can fully load the function in time */
+  // Now safely set the rewards content
+  const cartRewards = document.getElementById('rewards-cart');
+  cartRewards.innerHTML = cartRewardsHtml;
+
+  // Add the active class with a slight delay for transitions
   setTimeout(() => {
-    // This code toggles the transition and activates it when the "displayCart" function is called
     document.getElementById('cart-page').classList.add('active');
     document.getElementById('cart-buy').classList.add('active');
-  }, 10); // This delays the function by 10 milliseconds
+  }, 10);
 }
 
 function closeCart() {
   document.getElementById('cart-page').classList.remove('active');
   document.getElementById('cart-buy').classList.remove('active');
-
 }
