5a25d6b72404dc2cd6e0cb71a668b99d3966de4a
bluemario8 <bluemario812@gmail.com>
2024-11-26 10:10:16 -0500

    Reviews now add to local storage. If guest clicks create review, goes to account page, if not logged in, goes to login page. Overall Rating now updates properly, showing the number rating, rating in stars, and number of reviews

--- resources/js/reviews.js
+++ resources/js/reviews.js
@@ -1,10 +1,11 @@
 class ReviewItem
 {
-    constructor(name, date, rating, message) {
+    constructor(name, date, rating, message, email) {
         this.name = name;
         this.date = date;
         this.rating = rating;
         this.message = message;
+        this.email = email;
     }
 };
 
@@ -17,25 +18,25 @@ let createReviewRating = 5;
 
 const defaultReviews =
 [
-    new ReviewItem("Steven Hill", "6/11/2023", 5, `These burgers are the best
-    I've ever had. I've heard they were great but now I think that's an
-    understatement. If you've never been to Nick's Bar and Grill, you NEED to
-    go.`),
     new ReviewItem("Jessica Smith", "9/23/2024", 5, `Oh my this food tastes
     amazing. There's so many great food options to pick from and I want to try
     them all. Each time I try a new food, it tastes better than the last. `),
-    new ReviewItem("Jack Stevens", "5/30/2023", 5, `I was hungry and found this
-    place and now I don't know how I lived without it. The food, drinks, and
-    service were all incredible. Their spicy burger is 100% my favorite. `),
-    new ReviewItem("Harald Ulterfill", "5/23/2024", 4, `I've heard all the hype
-    of this place and was super excited to try it. It was good, but I definitely
-    isn't as good as it is hyped to be. Overall, still great food, but not
-    absured.`),
     new ReviewItem("Amy Davidson", "6/04/2024", 2, `The food was good, but I had
     a bad impression of the staff. It took a while for me to get my food, they
     kept interupting me, and they got my order wrong. Hopefully, this is a one
     off experience and maybe they were having a rough day, but it doesn't change
     the fact it was bad service`),
+    new ReviewItem("Harald Ulterfill", "5/23/2024", 4, `I've heard all the hype
+    of this place and was super excited to try it. It was good, but I definitely
+    isn't as good as it is hyped to be. Overall, still great food, but not
+    absured.`),
+    new ReviewItem("Steven Hill", "6/11/2023", 5, `These burgers are the best
+    I've ever had. I've heard they were great but now I think that's an
+    understatement. If you've never been to Nick's Bar and Grill, you NEED to
+    go.`),
+    new ReviewItem("Jack Stevens", "5/30/2023", 5, `I was hungry and found this
+    place and now I don't know how I lived without it. The food, drinks, and
+    service were all incredible. Their spicy burger is 100% my favorite. `),
 ];
 
 if (localStorage.getItem("reviews") === null)
@@ -49,17 +50,54 @@ for (let review of reviews)
     reviewList.innerHTML += genReviewHtml(review);
 }
 
+updateOverallRating();
+
+
+function updateOverallRating()
+{
+    const overallRatingElem = document.getElementsByClassName("overall-rating")[0];
+    const overallStarsElem = document.getElementsByClassName("overall-stars")[0];
+    const reviewsNumberElem = 
+        document.getElementsByClassName("reviews-number")[0].getElementsByTagName("a")[0];
+    let reviewCount = 0;
+    let totalReviewStars = 0;
+    let overallRating = 0;
+    let stars = 0;
+
+    for (let i in reviews)
+    {
+        totalReviewStars += reviews[i].rating;
+        reviewCount++;
+    }
+    overallRating = Math.round((totalReviewStars / reviewCount + Number.EPSILON) * 100) / 100;
+
+    stars = Math.round(overallRating);
+    console.log(genStars(stars));
+
+    overallRatingElem.innerText = overallRating;
+    overallStarsElem.innerHTML = genStars(stars);
+    reviewsNumberElem.innerText = reviewCount;
+    
+
+}
+
+function genStars(starNum)
+{
+    let starsHtml = ``;
+    for (let stars = 1; stars <= 5; ++stars) 
+    {
+        if (stars <= starNum)
+            starsHtml += `<ion-icon name="star" class="orange-star"></ion-icon>\n`;
+        else
+            starsHtml += `<ion-icon name="star"></ion-icon>\n`;
+    }
+
+    return starsHtml;
+}
 
 function genReviewHtml(obj)
 {
-    let reviewsStars = ``;
-    for (let stars = 1; stars <= 5; ++stars) 
-    {
-        if (stars <= obj.rating)
-            reviewsStars += `<ion-icon name="star" class="orange-star"></ion-icon>\n`;
-        else
-            reviewsStars += `<ion-icon name="star"></ion-icon>\n`;
-    }
+    let reviewsStars = genStars(obj.rating);
 
     return `
         <li class="rate-${obj.rating}">
@@ -71,7 +109,7 @@ function genReviewHtml(obj)
                 </div>
             </div>
 
-            <q>${obj.message}</q>
+            ${obj.message !== "" ? "<q>" + obj.message + "</q>" : ""}
         </li> `
 }
 
@@ -106,7 +144,18 @@ function createReview()
 {
     console.log("Creating Review!");
 
-    let reviewElemExist = document.getElementsByClassName("review-popup");
+    if (!loggedIn)
+    {
+        window.location.href = "login.html";
+        return;
+    }
+    if (loggedInUser === "guest")
+    {
+        window.location.href = "account.html";
+        return;
+    }
+
+    let reviewElemExist = document.getElementsByClassName("reviews-popup");
     let reviewElem = document.createElement("div");
 
     if (reviewElemExist.length >= 1)
@@ -114,7 +163,8 @@ function createReview()
 
     reviewElem.classList = "popup-home flex";
     reviewElem.innerHTML = `
-        <ul class="popup-box reviews-list flex">
+        <ul class="popup-box reviews-popup flex">
+            <ion-icon name="close-outline" class="pointer close-x reviews-fixed-x" onclick="closeReview()"></ion-icon>
             <h3>Create a Review!</h3>
             <li>
                 <label for="rating">Rating: </label>
@@ -145,7 +195,7 @@ function createReview()
 
     document.body.prepend(reviewElem);
 
-    // document.getElementsByClassName("review-popup")[0].getElementsByTagName("a")[0].focus();
+    // document.getElementsByClassName("reviews-popup")[0].getElementsByTagName("a")[0].focus();
 }
 
 function updateStarCount(data, num)
@@ -173,12 +223,17 @@ function updateStarCount(data, num)
 
 function submitReview(data)
 {
+    if (!loggedIn)
+        return;
+
     const date = new Date;
+    // review has name, date, rating, comment, and email
     const review = new ReviewItem(
         userData["firstName"] + ' ' +  userData["lastName"],
         `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`,
         createReviewRating,
-        document.getElementById("comment").value
+        document.getElementById("comment").value,
+        loggedInUser
     );
 
     // console.log("Rating is: " + createReviewRating);
@@ -188,5 +243,14 @@ function submitReview(data)
 
     console.log(review);
 
-    // reviewList.prepend()
+    reviews.unshift(review);
+
+    localStorage.setItem("reviews", JSON.stringify(reviews));
+
+    window.location.href = "reviews.html";
+}
+
+function closeReview()
+{
+    document.getElementsByClassName("popup-home")[0].remove();
 }
